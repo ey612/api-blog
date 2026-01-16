f31283c3c847883066470b92905ea00fe0d38f50
chaessi <chaessi514@gmail.com>
2026-01-16 17:24:54 +0900

    app 변수를 if 밖으로 이동

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def create_app():
     #DB 초기화
     db.init_app(app)
     
-    #Blueprint 등록 -- # Blueprint : 라우트를 그룹으로 묶는 방법
+    #Blueprint 등록
     from routes.auth import auth_bp
     from routes.posts import posts_bp
     from routes.comments import comments_bp
@@ -24,12 +24,15 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix='/api')
     app.register_blueprint(posts_bp, url_prefix='/api')
     app.register_blueprint(comments_bp, url_prefix='/api')
+    
     #테이블 생성
     with app.app_context():
         db.create_all()
         
     return app
 
+app = create_app()
+
+# 로컬 테스트용
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5000)
