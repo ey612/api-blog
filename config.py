f5f7ce2838cbcbf4f3294edcbd6d9d1aca317d61
chaessi <chaessi514@gmail.com>
2026-01-19 10:59:55 +0900

    환경 변수 설정 및 db변경

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # 환경 파일
+import os
 
 class Config:
     #SQLite 데이터베이스 경로
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
     #경고 메시지 끄기
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -11,4 +12,4 @@ class Config:
     JSON_AS_ASCII = False
     
     #JWT 시크릿 키
-    SECRET_KEY = 'your-secret-key-change-this'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
